d2d6b332cb7d89f6f838225c21a920e007035440
flekhulani <flekhulani@retrorabbit.co.za>
2019-03-04 21:54:12 +0200

    Added one controller and 4 classes for RestChecks.

---
+++ DummyCheckService/DummyCheckService/Checks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DummyCheckService
+{
+    public class CriminalCheck
+    {
+        public string name;
+        public string surname;
+        public int idNr;
+        public string[] CriminalRecords;
+    }
+
+    public class CreditCheck
+    {
+        public string name;
+        public string surname;
+        public int idNr;
+        public int creditScore;
+    }
+
+    public class AcademicCheck
+    {
+        public string name;
+        public string surname;
+        public int idNr;
+        public bool hasQualification;
+    }
+}
