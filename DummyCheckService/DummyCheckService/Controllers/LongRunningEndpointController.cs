7d37adc75352550e6df1093f4f8f5bf180a0437a
Mpinane <mmohale@retrorabbit.co.za>
2019-03-05 04:01:13 +0200

    generated json, sent http request to be tested with actual response servicce function

---
+++ DummyCheckService/DummyCheckService/Controllers/LongRunningEndpointController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DummyCheckService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LongRunningEndpointController : ControllerBase
+    {
+        public enum checkStatus { Active, Failed, PossibleIssues, Cleared, InProgress, NotStarted}
+
+        // POST: api/available/addService
+        [HttpGet]
+        [Route("theEndpoint")]
+        public ActionResult theEndpoint()
+        {
+
+            byte[] file = System.IO.File.ReadAllBytes(@"C:\Users\Mpinane Mohale\Downloads\32423.pdf");
+
+            dynamic result = new Newtonsoft.Json.Linq.JObject();
+            result.checkId = 1;
+            result.resultStatus = checkStatus.Cleared.ToString();
+            result.resultDesciption = "Check came back with no issues";
+            result.file = Convert.ToBase64String(file);
+
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("some api route");
+            webRequest.ContentType = "application/json";
+            webRequest.Method = "POST";
+            webRequest.KeepAlive = true;
+
+            //ASCIIEncoding encoding = new ASCIIEncoding();
+            //byte[] body = System.IO.GetBytes(result);
+
+            //webRequest.ContentLength = result.Length;
+
+            byte[] body = Encoding.UTF8.GetBytes(result.ToString());
+            Stream newStream = webRequest.GetRequestStream();
+            webRequest.Accept = "application/json";
+            webRequest.ContentLength = body.Length;
+            newStream.Write(body, 0, body.Length);
+            newStream.Close();
+            webRequest.GetResponse();
+
+            return Ok(webRequest.GetResponse()); 
+        }
+    }
+}
